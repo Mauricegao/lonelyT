8ae249965ec92f1ec943d4e6260f51d07018f7a9
Mauricegao <yunzhi@ualberta.ca>
2017-01-26 19:44:59 -0700

    lab3

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ImportantTweet.java
@@ -0,0 +1,19 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+
+public class ImportantTweet extends Tweet {
+    public ImportantTweet(String message) {
+        super(message);
+    }
+
+    public ImportantTweet(Date date, String message) {
+        super(date, message);
+    }
+
+    @Override
+    public Boolean isImportant(){
+        return true;
+    }
+}
